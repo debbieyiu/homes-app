c7033f615c290facafe6e01b5e5942828c1b3281
Debbie Yiu <debbie.yiu.0219@gmail.com>
2024-04-18 23:07:58 +0800

    adding home component to main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ HomeComponent ],
   template: `
     <main>
       <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
       </header>
+      <section>
+        <app-home></app-home>
+      </section>
     </main>
   `,
   styleUrls: ['./app.component.css'],
