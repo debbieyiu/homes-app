3f0517f706fa53cfa593357c2b6829557d82281b
Debbie Yiu <debbie.yiu.0219@gmail.com>
2024-04-18 23:11:05 +0800

    modify content of home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,9 +5,14 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+    </section>
   `,
   styleUrl: './home.component.css'
 })
