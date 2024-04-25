1da3d829c402705977aaf9e8a10cab07ca61ab4e
Debbie Yiu <debbie.yiu.0219@gmail.com>
2024-04-25 17:49:30 +0800

    add route to header logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,12 @@ import { RouterModule } from '@angular/router';
   imports: [ HomeComponent, RouterModule ],
   template: `
     <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-      </header>
-      <section>
+      <a [routerLink]="['/']">
+          <header class="brand-name">
+            <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+          </header>
+      </a>
+      <section class="content">
         <app-home></app-home>
       </section>
     </main>
