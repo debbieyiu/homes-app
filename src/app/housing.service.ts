b381dd05f70d9fcf2bd92cf8925c140522c76e69
Debbie Yiu <debbie.yiu.0219@gmail.com>
2024-04-25 19:03:11 +0800

    customize detail component and add service function getHousingLocationById()

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -114,4 +114,8 @@ export class HousingService {
   getAllHousingLocations(): HousingLocation[] {
     return this.housingLocationList;
   }
+
+  getHousingLocationById(id: number): HousingLocation | undefined  {
+    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
 }
