91d7a969c6945ce3d73d525c0bfa1df75827b131
Debbie Yiu <debbie.yiu.0219@gmail.com>
2024-04-25 12:18:56 +0800

    add import inject from @angular/core & import HousingService

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housing-location';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
