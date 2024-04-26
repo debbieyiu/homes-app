95f52f107518f49f36a89874ebc93533142e5476
Debbie Yiu <debbie.yiu.0219@gmail.com>
2024-04-26 14:39:33 +0800

    update the home component template

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,19 +11,20 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
         [housingLocation]="housingLocation">
       </app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
